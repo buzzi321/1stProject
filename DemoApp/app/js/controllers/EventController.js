d584db642581cd045146398c69f937183a2f1013
Sekhar Gogineni <sekhar.g@gmail.com>
2016-04-28 11:03:33 -0700

    test

---
+++ DemoApp/app/js/controllers/EventController.js
@@ -0,0 +1,31 @@
+'use strict';
+
+eventsApp.controller('EventController',
+    function EventController($scope, eventData, $log, $anchorScroll) {
+        $scope.snippet = '<span style="color:red">hi there</span>';
+        $scope.boolValue = false;
+        $scope.mystyle = {color: 'red'};
+        $scope.myclass = "blue";
+        $scope.buttonDisabled = true;
+        $scope.sortorder = 'name';
+        eventData.getEvents()
+            .$promise
+            .then(function(event) {$scope.event = event; console.log(event);})
+            .catch(function(response) {console.log(response);});
+
+
+
+        $scope.upVoteSession=function(session){
+            session.upVoteCount++;
+        };
+        $scope.downVoteSession=function(session){
+            session.upVoteCount--;
+        };
+
+        $scope.scrollToSession=function(){
+            $anchorScroll();
+        }
+    }
+);
+
+
